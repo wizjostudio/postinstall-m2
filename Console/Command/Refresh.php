f83c6717425b3b29bd700ad662e3a2e5876aed1c
rzeka <piotr@rzeka.net>
2019-08-02 13:46:30 +0200

    Fix contents of generated not being removed

--- Console/Command/Refresh.php
+++ Console/Command/Refresh.php
@@ -5,7 +5,6 @@ namespace Wizjo\Postinstall\Console\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Filesystem\Filesystem;
 
 class Refresh extends SetupAbstract
 {
@@ -23,17 +22,34 @@ class Refresh extends SetupAbstract
     {
         $magento = $input->getOption('magento');
 
-        $commands = [
-            [
-                'command' => 'rm',
-                'args' => [
-                    sprintf(
-                        '%s/generated/*',
-                        $magento
-                    ),
-                ],
-                'type' => self::COMMAND_TYPE_SYSTEM,
-            ],
+        $commands = [];
+        $composerCommand = [
+            'command' => 'composer',
+            'args' => ['dump-autoload'],
+        ];
+
+        foreach (['code', 'metadata'] as $subdir) {
+            $path = sprintf(
+                '%s/generated/%s',
+                $magento,
+                $subdir
+            );
+
+            if (file_exists($path) && is_dir($path)) {
+                $commands[] = [
+                    'command' => 'rm',
+                    'options' => ['-rf'],
+                    'args' => [$path],
+                    'type' => self::COMMAND_TYPE_SYSTEM,
+                ];
+            }
+        }
+
+        $commands[] = $composerCommand;
+
+        $commands[] = [
+            'command' => 'setup:upgrade',
+            'type' => self::COMMAND_TYPE_MAGENTO,
         ];
 
         if (!$input->getOption('no-recompile')) {
@@ -41,12 +57,9 @@ class Refresh extends SetupAbstract
                 'command' => 'setup:di:compile',
                 'type' => self::COMMAND_TYPE_MAGENTO,
             ];
-        }
 
-        $commands[] = [
-            'command' => 'setup:upgrade',
-            'type' => self::COMMAND_TYPE_MAGENTO,
-        ];
+            $commands[] = $composerCommand;
+        }
 
         $commands[] = [
             'command' => 'cache:flush',
